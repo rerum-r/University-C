b0c1fb80518fb66e0d1847270745ecb9e175fe29
Roman Notkov <46577408+rerum-r@users.noreply.github.com>
2019-11-04 16:38:10 +0300

    Chapter 1.5.2 Character Counting
    Chapter 1.5.2 Character Counting

--- My_first_application.c
+++ My_first_application.c
@@ -1,13 +1,47 @@
 #include <stdio.h>
 
-/* вывод таблицы температур по Фаренгейту и Цельсию  начинаю с 300*/
+/* вывод тамблицы температур по Фаренгейту и Цельсию
+   версия с вещественной арифметикой*/
 
 int main()
 {
-    int fahr;
+    float celsius, fahr;
+    int lower, upper, step;
 
-    printf("Temperature Table\n"); /* Заголовок таблицы */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
 
-    for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+    celsius = LOWER;
+    printf("Temperature Table Celsius to Fahrenheit\n");
+    while (celsius <= UPPER) {
+        fahr = 9.0/5.0 * celsius+32.0;
+        printf ("%6.1f %5.4f\n", celsius, fahr);
+        celsius = celsius + STEP;
+    }
+
+    printf("\nTemperature Table Farenhate to Celsius.\n\n");
+
+    for (int fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
         printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    int c;
+    int counter = 0;
+    printf("\nCounter Chars in string\n");
+    printf("Constant EOF: %d\n", EOF);
+    while ((c = getchar()) != '\n') {
+        ++counter;
+        putchar(c);
+    }
+    printf("\nWhile statement C != EOF is: %d\n", c == EOF);
+    printf("Counter is: %d\n", counter);
+
+    printf("\nCharacter Counting program\n");
+    double nc;
+    for (nc =0; getchar() != EOF; ++nc)
+        ;
+    printf("%.0f\n", nc);
+
+
+    return 0;
 }
